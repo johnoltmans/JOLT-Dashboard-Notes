719bf5abc49a6512b4a4f650035ed720aa7dd74f
John O. <j.a.oltmans@gmail.com>
2025-05-28 22:15:08 +0200

    Update my-notes-plugin.php

--- my-notes-plugin.php
+++ my-notes-plugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: JOLT Dashboard Notes
+Plugin Name: JOLT™ Dashboard Notes
 Plugin URI: https://github.com/Smoshed/WordPress-Plugins
 Description: A simple plugin for personal notes in the admin dashboard.
-Version: 1.2
+Version: 1.3
 Author: John Oltmans
 Author URI: https://www.johnoltmans.nl/
 */
