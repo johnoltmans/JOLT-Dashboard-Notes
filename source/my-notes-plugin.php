2fe7dfe8b660b84690d557ea9aceb7c92fedbcae
John O. <johnoltmans@proton.me>
2025-08-20 14:42:02 +0200

    1.6

--- source/my-notes-plugin.php
+++ source/my-notes-plugin.php
@@ -4,10 +4,29 @@ Plugin Name: JOLT Dashboard Notes
 Plugin URI: https://github.com/johnoltmans/JOLT-Dashboard-Notes
 Description: A simple plugin for personal notes in the admin dashboard.
 Version: 1.6
+Requires at least: 6.8
+Requires PHP: 7.4
 Author: John Oltmans
 Author URI: https://www.johnoltmans.nl/
 License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: simple-plugin-for-personal-notes-by-john-oltmans
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 if (!defined('ABSPATH')) {
